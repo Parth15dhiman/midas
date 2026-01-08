58ba996d8a98378302d33c00ce1935052736b7ac
Parth _ <3315.parth.com>
2026-01-08 12:20:49 +0530

    update project

--- src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
+++ src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
@@ -1,12 +1,19 @@
 package com.jpmc.midascore.component;
 
 import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.foundation.Transaction;
 import com.jpmc.midascore.repository.UserRepository;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DatabaseConduit {
     private final UserRepository userRepository;
+
+    @KafkaListener(topics = "${general.kafka-topic}", groupId = "midas-core-group")
+    public void listen(Transaction transaction){
+        System.out.println("Received transaction " + transaction);
+    }
 
     public DatabaseConduit(UserRepository userRepository) {
         this.userRepository = userRepository;
